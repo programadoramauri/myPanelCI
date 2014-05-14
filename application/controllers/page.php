17be9a7eb1bfd2c7e50cf09b7aea7d70fee91025
Amauri Franco <programador.amauri@gmail.com>
2014-05-14 14:14:30 -0300

    Creating basic model

---
+++ application/controllers/page.php
@@ -0,0 +1,25 @@
+<?php
+class Page extends Frontend_Controller{
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('page_m');
+	}
+
+	public function index(){
+		$pages = $this->page_m->get(1);
+		var_dump($pages);
+	}
+
+	public function save(){
+		$data = array(
+			'title' => 'My Fuckin Page',
+			'slug' => 'my-fuckin-page',
+		);
+		$id = $this->page_m->save($data, 3);
+		var_dump($id);
+	}
+	public function delete(){
+		$this->page_m->delete(3);
+	}
+
+}
--- application/libraries/Frontend_Controller.php
+++ application/libraries/Frontend_Controller.php
@@ -2,7 +2,6 @@
 class Frontend_Controller extends MY_Controller{
 	public function __construct(){
 		parent::__construct();
-		var_dump('Wellcome from my frontend controller');
 	}
 
 }
